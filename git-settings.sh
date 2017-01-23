00fc4187159a7e8d96946493a20b73fb112de770
gseng-test <gokhan.sengun@gmail.com>
2017-01-23 23:35:28 +0200

    Delete git-settings.sh

--- git-settings.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export GIT_AUTHOR_NAME="Gökhan Şengün"
-export GIT_AUTHOR_EMAIL="gokhansengun@gmail.com"
-export GIT_COMMITTER_NAME="Gökhan Şengün"
-export GIT_COMMITTER_EMAIL="gokhansengun@gmail.com"
